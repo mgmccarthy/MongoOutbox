b07de152d84834ce562c2fc27001c86203500950
Mike McCarthy <mike_mccarthy3@yahoo.com>
2021-06-17 12:53:31 -0400

    Add empty OrderCreatedHanlder

---
+++ src/MongoOutbox.Endpoint2/OrderCreatedHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using MongoOutbox.Shared;
+using NServiceBus;
+
+namespace MongoOutbox.Endpoint2
+{
+    public class OrderCreatedHandler : IHandleMessages<OrderCreated>
+    {
+        public Task Handle(OrderCreated message, IMessageHandlerContext context)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
