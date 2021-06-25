24a0e75a324b3593dd41358fedd4648853302177
Mike McCarthy <mike_mccarthy3@yahoo.com>
2021-06-25 17:14:13 -0400

    Add SampleSaga to see example of Saga data storage in mongo

--- src/MongoOutbox.Client/Program.cs
+++ src/MongoOutbox.Client/Program.cs
@@ -28,15 +28,15 @@ namespace MongoOutbox.Client
 
             var endpointInstance = await Endpoint.Start(endpointConfiguration).ConfigureAwait(false);
 
-            //wait 10 seconds before dispatching messages so Endpoint1 can stand up and create the rabbit topology
+            //wait 10 seconds before dispatching messages so Endpoint1 and Endpoint 2 can stand up and create their rabbit toplogies
             await Task.Delay(10000);
 
-            //await CreateOrder(endpointInstance);
-            while (true)
-            {
-                await CreateOrder(endpointInstance);
-                await Task.Delay(5000);
-            }
+            await CreateOrder(endpointInstance);
+            //while (true)
+            //{
+            //    await CreateOrder(endpointInstance);
+            //    await Task.Delay(5000);
+            //}
         }
 
         private static async Task CreateOrder(IEndpointInstance endpointInstance)
---
+++ src/MongoOutbox.Endpoint1/SampleSaga.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using MongoOutbox.Shared;
+using NServiceBus;
+using NServiceBus.Logging;
+
+namespace MongoOutbox.Endpoint1
+{
+    public class SampleSaga : Saga<SampleSaga.SampleSagaData>, 
+        IAmStartedByMessages<OrderCreated>, 
+        IHandleTimeouts<SampleSaga.TimeoutState>
+
+    {
+        private static readonly ILog Log = LogManager.GetLogger<SampleSaga>();
+
+        protected override void ConfigureHowToFindSaga(SagaPropertyMapper<SampleSagaData> mapper)
+        {
+            mapper.ConfigureMapping<OrderCreated>(message => message.Order.Id).ToSaga(sagaData => sagaData.OrderId);
+        }
+
+        public Task Handle(OrderCreated message, IMessageHandlerContext context)
+        {
+            Log.Info($"SampleSaga started with OrderId: {message.Order.Id}");
+            Log.Info($"Setting timeout for 15 seconds");
+            return RequestTimeout<TimeoutState>(context, TimeSpan.FromSeconds(15));
+        }
+
+        public Task Timeout(TimeoutState state, IMessageHandlerContext context)
+        {
+            Log.Info($"SampleSaga timeout fired, marking saga complete");
+            MarkAsComplete();
+            return Task.CompletedTask;
+        }
+
+        public class SampleSagaData : ContainSagaData
+        {
+            public Guid OrderId { get; set; }
+        }
+
+        public class TimeoutState { }
+    }
+}
